9eae457eb102312c5c2df90ef19fab4aaa15c921
jiehui555 <jiehui555@qq.com>
2026-06-23 06:01:23 +0000

    chore: 更新版本号至 1.5.2

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		// 初始化数据库
 		database.Init(options.DBPath)
 		router := http.NewServeMux()
-		api := humago.New(router, huma.DefaultConfig("我的 API", "1.5.1"))
+		api := humago.New(router, huma.DefaultConfig("我的 API", "1.5.2"))
 
 		// 注册路由
 		handler.RegisterGreeting(api)
